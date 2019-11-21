bb2cdc5e11f6e6aef929f61d390fcb845c9446b2
Orbis2358 <mgkid3310@naver.com>
2019-11-21 20:44:53 +0900

    add token func

---
+++ KIN3_Esi.py
@@ -0,0 +1,19 @@
+from esipy import EsiApp
+from esipy import EsiClient
+from esipy import EsiSecurity
+
+def add_token(app, security, client, code, file = './esi_tokens.txt'):
+	try:
+		token = security.auth(code)
+	except Exception as error:
+		return str(error)
+	refresh_token = token['refresh_token']
+
+	api_info = security.verify()
+	operation = app.op['get_characters_character_id'](character_id = api_info['sub'].split(':')[-1])
+	name = client.request(operation).data['name']
+
+	with open(file, 'a') as file:
+		file.write(f'{name}:{refresh_token}\n')
+
+	return ''
