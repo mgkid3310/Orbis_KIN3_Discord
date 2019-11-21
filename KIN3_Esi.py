2538f05644e46a367030976def1d1fa8aeb01a09
Orbis2358 <mgkid3310@naver.com>
2019-11-21 21:28:17 +0900

    Update KIN3_Esi.py

--- KIN3_Esi.py
+++ KIN3_Esi.py
@@ -2,7 +2,7 @@ from esipy import EsiApp
 from esipy import EsiClient
 from esipy import EsiSecurity
 
-def add_token(app, security, client, code, user_id = -1, file_path = './esi_tokens.txt'):
+def add_token(app, security, client, code, discord_id = -1, file_path = './esi_tokens.txt'):
 	try:
 		token = security.auth(code)
 	except Exception as error:
@@ -18,8 +18,8 @@ def add_token(app, security, client, code, user_id = -1, file_path = './esi_toke
 	with open(file_path, 'w') as file:
 		for line in lines:
 			read_id = int(line.strip().split(":")[2])
-			if read_id < 0 or read_id != user_id:
+			if read_id < 0 or read_id != discord_id:
 				file.write(line)
-		file.write(f'{name}:{refresh_token}:{user_id}\n')
+		file.write(f'{name}:{refresh_token}:{discord_id}\n')
 
 	return ''
