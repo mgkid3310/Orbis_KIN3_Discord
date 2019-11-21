f6abd2fbae0f9648f04a22df8f75e293bb747921
Orbis2358 <mgkid3310@naver.com>
2019-11-21 21:18:40 +0900

    remove duplicated codes

--- KIN3_Esi.py
+++ KIN3_Esi.py
@@ -2,7 +2,7 @@ from esipy import EsiApp
 from esipy import EsiClient
 from esipy import EsiSecurity
 
-def add_token(app, security, client, code, user_id = -1, file = './esi_tokens.txt'):
+def add_token(app, security, client, code, user_id = -1, file_path = './esi_tokens.txt'):
 	try:
 		token = security.auth(code)
 	except Exception as error:
@@ -13,7 +13,13 @@ def add_token(app, security, client, code, user_id = -1, file = './esi_tokens.tx
 	operation = app.op['get_characters_character_id'](character_id = api_info['sub'].split(':')[-1])
 	name = client.request(operation).data['name']
 
-	with open(file, 'a') as file:
+	with open(file_path, "r") as file:
+		lines = file.readlines()
+	with open(file_path, 'w') as file:
+		for line in lines:
+			read_id = int(line.strip().split(":")[2])
+			if read_id < 0 or read_id != user_id:
+				file.write(line)
 		file.write(f'{name}:{refresh_token}:{user_id}\n')
 
 	return ''
