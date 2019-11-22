bc3ff944c42d69ca61bb5450e8648dcbec2542f3
Orbis2358 <mgkid3310@naver.com>
2019-11-22 16:51:57 +0900

    Update KIN3_waitlist.py

--- KIN3_waitlist.py
+++ KIN3_waitlist.py
@@ -97,11 +97,11 @@ class server_list:
 			width_dps = 5 + len(str(self.waitcount_dps))
 			width_snp = 5 + len(str(self.waitcount_snp))
 			if self.waitcount_dps > 0:
-				width_dps_list = [len(x.display_name) for x in self.waitlist_dps]
+				width_dps_list = [len(x.discord_member.display_name) for x in self.waitlist_dps]
 				width_dps_list.append(width_dps)
 				width_dps = max(width_dps_list)
 			if self.waitcount_snp > 0:
-				width_snp_list = [len(x.display_name) for x in self.waitlist_snp]
+				width_snp_list = [len(x.discord_member.display_name) for x in self.waitlist_snp]
 				width_snp_list.append(width_snp)
 				width_snp = max(width_snp_list)
 			width_dps += 4
@@ -116,17 +116,17 @@ class server_list:
 				dps_name, snp_name, logi_name = '', '', ''
 				dps_white, snp_space = ' ' * width_dps, ' ' * width_snp
 				if index < self.waitcount_dps:
-					dps_name = self.waitlist_dps[index].display_name
+					dps_name = self.waitlist_dps[index].discord_member.display_name
 					dps_white = ' ' * (width_dps - len(dps_name))
 				if index < self.waitcount_snp:
-					snp_name = self.waitlist_snp[index].display_name
+					snp_name = self.waitlist_snp[index].discord_member.display_name
 					snp_space = ' ' * (width_snp - len(snp_name))
 				if index < self.waitcount_logi:
-					logi_name = self.waitlist_logi[index].display_name
+					logi_name = self.waitlist_logi[index].discord_member.display_name
 				text += '\n' + dps_name + dps_white + snp_name + snp_space + logi_name
 			text += '\n\n----------------'
 			for request in self.request_list:
-				text += f'\n{request[0].display_name}이(가)'
+				text += f'\n{request[0].discord_member.display_name}이(가)'
 				if request[1] > 0:
 					text += f' {request[1]} DPS,'
 				if request[2] > 0:
