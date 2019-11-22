ffb150c8293a0caf434595df3a7f3caf4654b73a
Orbis2358 <mgkid3310@naver.com>
2019-11-22 16:06:34 +0900

    change instance name

--- KIN3_Esi.py
+++ KIN3_Esi.py
@@ -1,10 +1,10 @@
 class eve_character:
 	def __init__(self, esi_objects, input):
-		name, eve_char_id, refresh_token, discord_id = input
+		name, char_id, refresh_token, discord_id = input
 
 		self.esi_objects = esi_objects
 		self.name = name
-		self.eve_char_id = eve_char_id
+		self.char_id = char_id
 		self.refresh_token = refresh_token
 		self.discord_id = discord_id
 
@@ -31,7 +31,7 @@ class eve_character:
 			'refresh_token': self.refresh_token
 		})
 		token = security.refresh()
-		operation = app.op['get_characters_character_id_online'](character_id = self.eve_char_id)
+		operation = app.op['get_characters_character_id_online'](character_id = self.char_id)
 		is_online = client.request(operation).data['online']
 
 		return is_online
@@ -45,7 +45,7 @@ class eve_character:
 			'refresh_token': self.refresh_token
 		})
 		token = security.refresh()
-		operation = app.op['get_characters_character_id_location'](character_id = self.eve_char_id)
+		operation = app.op['get_characters_character_id_location'](character_id = self.char_id)
 		location = client.request(operation).data
 
 		return location
@@ -59,7 +59,7 @@ class eve_character:
 			'refresh_token': self.refresh_token
 		})
 		token = security.refresh()
-		operation = app.op['get_characters_character_id_fittings'](character_id = self.eve_char_id)
+		operation = app.op['get_characters_character_id_fittings'](character_id = self.char_id)
 		location = client.request(operation).data
 
 		return location
@@ -73,7 +73,7 @@ class eve_character:
 			'refresh_token': self.refresh_token
 		})
 		token = security.refresh()
-		operation = app.op['get_characters_character_id_fleet'](character_id = self.eve_char_id)
+		operation = app.op['get_characters_character_id_fleet'](character_id = self.char_id)
 		fleet_data = client.request(operation).data
 
 		return fleet_data
