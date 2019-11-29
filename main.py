5ac2d4aeb04497a784f06f6301e7d05a580f5e6f
Orbis2358 <mgkid3310@naver.com>
2019-11-29 10:31:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -150,26 +150,26 @@ async def on_message(message):
 
 	# x up
 	if prefix in ['x', 'ㅌ']:
-		auth_count = KIN3_database.auth_count(message.author)
-		if not auth_count > 0:
-			await waitlist.xup_channel.send(f'{display_name}, 계정이 등록되지 않은 유저입니다, 계정 인증이 필요합니다')
-			await message.channel.send(embed = auth_embed)
-			return None
-		else:
-			if auth_count > 1 and char_index is None:
-				await waitlist.xup_channel.send(f'{display_name}, 등록된 계정이 두개 이상입니다. 계정을 특정해주세요')
-				await waitlist.xup_channel.send(KIN3_database.xup_selection_info(message.author))
-				return None
-
-			eve_char_object = KIN3_database.get_character_object(esi_objects, message.author, char_index)
-			if eve_char_object is None:
-				await waitlist.xup_channel.send(f'{display_name}, 에러가 발생했습니다. 관리자에게 문의해주세요\n에러코드: KIN3_database 101, character object init fail')
-				return None
-
 		if waitlist.xup_channel is None:
 			waitlist.xup_channel = channel
 
 		if channel == waitlist.xup_channel:
+			auth_count = KIN3_database.auth_count(message.author)
+			if not auth_count > 0:
+				await waitlist.xup_channel.send(f'{display_name}, 계정이 등록되지 않은 유저입니다, 계정 인증이 필요합니다')
+				await waitlist.xup_channel.send(embed = auth_embed)
+				return None
+			else:
+				if auth_count > 1 and char_index is None:
+					await waitlist.xup_channel.send(f'{display_name}, 등록된 계정이 두개 이상입니다. 계정을 특정해주세요')
+					await waitlist.xup_channel.send(KIN3_database.xup_selection_info(message.author))
+					return None
+
+				eve_char_object = KIN3_database.get_character_object(esi_objects, message.author, char_index)
+				if eve_char_object is None:
+					await waitlist.xup_channel.send(f'{display_name}, 에러가 발생했습니다. 관리자에게 문의해주세요\n에러코드: KIN3_database 101, character object init fail')
+					return None
+
 			roles = command.split(' ')
 
 			if len(keywords_dps.intersection(roles)) > 0:
@@ -206,7 +206,7 @@ async def on_message(message):
 			auth_count = KIN3_database.auth_count(message.author)
 			if not auth_count > 0:
 				await waitlist.xup_channel.send(f'{display_name}, 계정이 등록되지 않은 유저입니다, 계정 인증이 필요합니다')
-				await message.channel.send(embed = auth_embed)
+				await waitlist.xup_channel.send(embed = auth_embed)
 				return None
 			else:
 				if auth_count > 1 and char_index is None:
