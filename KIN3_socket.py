b560a20556297e58083836c23f5ced368bc03294
Orbis2358 <mgkid3310@naver.com>
2019-11-30 14:03:39 +0900

    round time

--- KIN3_socket.py
+++ KIN3_socket.py
@@ -6,23 +6,23 @@ import KIN3_database
 import KIN3_waitlist
 
 async def start_tcp_server(bot, tcp_server = None):
-	print(f'{time.time()} : Starting TCP server')
+	print(f'{round(time.time(), 4)} : Starting TCP server')
 
 	if tcp_server is not None:
-		print(f'{time.time()} : Server already existing')
-		print(f'{time.time()} : --------')
+		print(f'{round(time.time(), 4)} : Server already existing')
+		print(f'{round(time.time(), 4)} : --------')
 		return tcp_server
 
-	print(f'{time.time()} : No server existing')
+	print(f'{round(time.time(), 4)} : No server existing')
 	with open('./tcp_setup.txt', 'r') as file:
 		readRines = file.readlines()
 
 	tcp_server_ip = readRines[0].strip().split(':')
 	tcp_server = await asyncio.start_server(handle_tcp_inbound, tcp_server_ip[0], tcp_server_ip[1])
 	tcp_addr = tcp_server.sockets[0].getsockname()
-	print(f'{time.time()} : Server started on {tcp_addr}')
-	print(f'{time.time()} : Request handling started')
-	print(f'{time.time()} : --------')
+	print(f'{round(time.time(), 4)} : Server started on {tcp_addr}')
+	print(f'{round(time.time(), 4)} : Request handling started')
+	print(f'{round(time.time(), 4)} : --------')
 
 	bot.loop.create_task(start_serve(tcp_server))
 
@@ -39,23 +39,23 @@ async def handle_tcp_inbound(reader: asyncio.StreamReader, writer: asyncio.Strea
 	message = data.decode()
 	# sock.getpeername()
 
-	print(f'{time.time()} : [S]Received {message!r} from {addr!r}')
-	print(f'{time.time()} : [S]Echoing: {message!r}')
+	print(f'{round(time.time(), 4)} : [S]Received {message!r} from {addr!r}')
+	print(f'{round(time.time(), 4)} : [S]Echoing: {message!r}')
 	writer.write(data)
 	await writer.drain()
 
-	print(f'{time.time()} : [S]Close the connection')
+	print(f'{round(time.time(), 4)} : [S]Close the connection')
 	writer.close()
 	await writer.wait_closed()
 
 async def test_tcp_server():
-	print(f'{time.time()} : Testing TCP server')
+	print(f'{round(time.time(), 4)} : Testing TCP server')
 
 	message = 'hello world apple banana'
 	fs = [asyncio.ensure_future(start_tcp_test_client(word)) for word in message.split()]
 	await asyncio.wait(fs, timeout = 4)
-	print(f'{time.time()} : TCP server test complete')
-	print(f'{time.time()} : --------')
+	print(f'{round(time.time(), 4)} : TCP server test complete')
+	print(f'{round(time.time(), 4)} : --------')
 
 async def start_tcp_test_client(message: str):
 	reader: asyncio.StreamReader
@@ -67,14 +67,14 @@ async def start_tcp_test_client(message: str):
 	tcp_server_ip = readRines[0].strip().split(':')
 	reader, writer = await asyncio.open_connection(tcp_server_ip[0], tcp_server_ip[1])
 
-	print(f'{time.time()} : [C]Connected')
+	print(f'{round(time.time(), 4)} : [C]Connected')
 	writer.write(message.encode())
 	await writer.drain()
-	print(f'{time.time()} : [C]Send: {message!r}')
+	print(f'{round(time.time(), 4)} : [C]Send: {message!r}')
 
 	data = await reader.read(100)
-	print(f'{time.time()} : [C]Received: {data.decode()!r}')
+	print(f'{round(time.time(), 4)} : [C]Received: {data.decode()!r}')
 
-	print(f'{time.time()} : [C]Closing...')
+	print(f'{round(time.time(), 4)} : [C]Closing...')
 	writer.close()
 	await writer.wait_closed()
--- main.py
+++ main.py
@@ -20,15 +20,15 @@ async def on_ready():
 	global tcp_server
 
 	await bot.change_presence(activity = discord.Game(name = 'KIN3', type = 1))
-	print(f'{time.time()} : Bot logged in as')
-	print(f'{time.time()} : name : {bot.user.name}')
-	print(f'{time.time()} : id : {bot.user.id}')
+	print(f'{round(time.time(), 4)} : Bot logged in as')
+	print(f'{round(time.time(), 4)} : name : {bot.user.name}')
+	print(f'{round(time.time(), 4)} : id : {bot.user.id}')
 
 	# start periodic bot loop
 	bot.loop.create_task(event_periodic_1s())
 	bot.loop.create_task(event_periodic_60s())
-	print(f'{time.time()} : Periodic bot loop started')
-	print(f'{time.time()} : --------')
+	print(f'{round(time.time(), 4)} : Periodic bot loop started')
+	print(f'{round(time.time(), 4)} : --------')
 
 	# start tcp server
 	tcp_server = await KIN3_socket.start_tcp_server(bot, tcp_server)
@@ -306,7 +306,7 @@ async def event_periodic_60s():
 
 # setup main
 app = EsiApp().get_latest_swagger
-print(f'{time.time()} : EsiApp loaded')
+print(f'{round(time.time(), 4)} : EsiApp loaded')
 
 auth_key_file = open('./esi_auth_key.txt', 'r')
 auth_key_lines = auth_key_file.readlines()
@@ -320,7 +320,7 @@ security = EsiSecurity(
 	client_id = client_id,
 	secret_key = secret_key
 )
-print(f'{time.time()} : EsiSecurity loaded')
+print(f'{round(time.time(), 4)} : EsiSecurity loaded')
 
 client = EsiClient(
 	headers = {'User-Agent':'something'},
@@ -328,8 +328,8 @@ client = EsiClient(
 	header = {'User-Agent': 'Something CCP can use to contact you and that define your app'},
 	security = security
 )
-print(f'{time.time()} : EsiClient loaded')
-print(f'{time.time()} : --------')
+print(f'{round(time.time(), 4)} : EsiClient loaded')
+print(f'{round(time.time(), 4)} : --------')
 
 esi_objects = (app, security, client)
 
@@ -358,5 +358,5 @@ auth_embed = discord.Embed(title = '계정등록 링크', url = auth_url, descri
 server_list = KIN3_waitlist.server_list()
 tcp_server = None
 
-print(f'{time.time()} : Starting bot')
+print(f'{round(time.time(), 4)} : Starting bot')
 bot.run(bot_token)
