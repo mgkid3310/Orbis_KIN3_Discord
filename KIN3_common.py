45c363d3a976c35df52a15e21fa09ce6cbce4759
Orbis2358 <mgkid3310@naver.com>
2019-12-01 13:20:54 +0900

    update timestamp format

--- KIN3_common.py
+++ KIN3_common.py
@@ -1,4 +1,11 @@
 import time
 
 def timestamp():
-	return time.strftime('%c', time.localtime(time.time()))
+	time_now = time.time()
+	timestamp = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time_now))
+
+	decimal = str(round(time_now % 1, 4))[2:]
+	padding = '0' * max((0, 4 - len(decimal)))
+	timestamp += f'.{decimal}{padding}'
+
+	return timestamp
